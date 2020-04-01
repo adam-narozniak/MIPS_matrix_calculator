55c2cfba72a356353c5b28f2179d7ff88185d41f
Adam <narozniak.adam4@gmail.com>
2020-04-02 00:05:12 +0200

    With function to read matrix's sizes

--- matrix_calculator.asm
+++ matrix_calculator.asm
@@ -3,6 +3,7 @@
 greeting: 	.asciiz "It's a matrix calculator!\n"
 file_path:	.asciiz "/home/adam/Desktop/MIPS/code/data.txt"
 error_message:	.asciiz "Error: opening file\nCheck file path!\n"
+endl:		.asciiz "\n"
 if_testing:	.asciiz "Loop testing\n"
 plus:		.asciiz "+\n"
 minus:		.asciiz "-\n"
@@ -26,7 +27,7 @@ open_f:
 read_f:
 	li $v0,14
 	move $a0, $s0		#$a0 = file descriptor
-	la $a1, buff		#$a0 = buff adress
+	la $a1, buff		#$10 = buff adress
 	li $a2, 1024		#max number of char to read
 	syscall
 	
@@ -35,12 +36,32 @@ read_f:
 	move $a0, $s1
 	syscall	
 	
-	la $t0,buff		#testing buffor output
-	lb $t1, ($t0)
-	subi $t1,$t1,0x30
+	li $v0,4		#printing endl
+	la $a0, endl
+	syscall	
+	
+	move $s6,$zero 		#number of bytes read from file stored in $s6
+	
+	jal get_m_n
+	move $s2,$s7		#m1 - first matrix
+	jal get_m_n
+	move $s3,$s7		#n1 - first matrix
+	
+	#------------------------
+	#la $t0,buff-redundant	#testing buffor output
+	#lb $t1, buff
+	#subi $t1,$t1,'0'
 	li $v0,1
-	move $a0, $t1
+	move $a0, $s2
 	syscall
+	li $v0,4		#printing endl
+	la $a0, endl
+	syscall	
+	li $v0,1
+	move $a0, $s3
+	syscall
+	#------------------
+	
 	
 
 close_f:
@@ -55,6 +76,49 @@ end_err:
 		
 end:	li $v0,10		#exit
 	syscall
+
+	
+#---------------------------------------------------------
+#function to get matrx size
+#$t0 holds result during counting and later on moves it to $s2 
+get_m_n:
+
+	la $t9, buff		#load buf address
+	add $t9,$t9,$s6
+	addiu $s6,$s6,1
+				#first char
+				
+	lbu $t0, ($t9)		#load first char
+	subi $t0,$t0,'0'	#convert to int
+	
+	#move $t8,$zero		
+new_char:
+	addi $s6,$s6,1	
+	#addiu $t8,$t8,1		#loop counter
+	#mulo $t7,$t7, $t8	#
+	addiu $t9,$t9,1		#next bait in buf
+	#lb $t1, 4(buff)
+	lbu $t1,($t9)
+	beqz $t1,end_int	#check if there are more to baits to read
+	beq $t1,'\n',end_int
+	beq $t1,' ',end_int
+	subi $t1,$t1,'0'
+	mulo $t0,$t0,10
+	add $t0,$t0,$t1
+	j new_char
+end_int:	
+	move $s7, $t0
+	move $t0,$zero
+	move $t1,$zero
+	jr $ra
+		
+#---------------------------------------------------------
+#function: int str_to_int(buf_adress, dynamic_memory_adress)
+#return value is stored in 
+#
+str_to_int:
+	
+
 	
 #	li $v0,1		#printing file descriptor
 #	move $a0, $t0
